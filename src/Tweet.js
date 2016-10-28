746222b5065d252a5093201e507cae2ab46a233e
Sonny Darvishzadeh <darvishzadeh@gmail.com>
2016-10-29 01:09:20 +0330

    Almost there <3

--- src/Tweet.js
+++ src/Tweet.js
@@ -10,19 +10,58 @@ class Tweet extends Component {
     this.state = {
       query: this.props.location.query.q,
       duration: this.props.location.query.duration,
-      autoplay: this.props.location.query.video
+      autoplay: this.props.location.query.video,
+      tweets: [{
+        id: 0,
+        name: "Dummy Name",
+        screen_name: "dummy_screen_name",
+        avatar: "dummyurl.jpg",
+        text: "Dummy text ipsum. #Hashtax"
+      }]
     };
   }
 
-  componentWillMount() {
-    fetch(`https://cloud.viewneo.com/social/twitter/search?q=${this.state.query}`)
-      .then( (response) => response.json())
-      .then((responseJson) => {
-        console.log(responseJson);
-      });
+  async getTweets() {
+    try {
+      let response = await fetch(`https://cloud.viewneo.com/social/twitter/search?q=${this.state.query}`);
+      let responseJson = await response.json();
+
+      return responseJson.statuses;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async componentWillMount() {
+    const fetchedTweets = await this.getTweets();
+
+    let tweets = fetchedTweets.map( (tweet) => {
+      return {
+        id: tweet.id,
+        name: tweet.user.name,
+        screen_name: tweet.user.screen_name,
+        avatar: tweet.user.profile_image_url,
+        text: tweet.text
+      };
+    });
+
+    this.setState( { tweets } );
+  }
+
+  async componentDidMount() {
+    // populate();
   }
 
   render() {
+    let tweets = this.state.tweets.map( (tweet) => {
+      return <div>
+          <p key={tweet.id}>
+            <img src={tweet.avatar} alt="Picture of {tweet.name}" /> {tweet.name} @{tweet.screen_name}<br />
+            {tweet.text}
+          </p><hr />
+        </div>;
+    });
+
     return (
       <div className="Tweet">
         <div className="Tweet-header">
@@ -40,6 +79,13 @@ class Tweet extends Component {
           Duration: {this.state.duration}<br />
           Video: {this.state.autoplay}<br />
         </p>
+
+        <hr />
+
+        <div>
+          {tweets}
+        </div>
+
       </div>
     );
   }
